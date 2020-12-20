c4647a1226dde8e7ed742aed4ab008dc15a63545
Nakano0921 <ken.voyaga@icloud.com>
2020-12-21 01:25:40 +0900

    関数化の一旦のコミット

--- lesson1/config.py
+++ lesson1/config.py
@@ -1,11 +1,7 @@
 # 一休レストランのTOPページのurl
 top_url = 'https://restaurant.ikyu.com/'
-# 「検索する」ボタンのxpath
-search_xpath = '//*[@id="__layout"]/div/main/div[2]/section[1]/div[2]/button'
-# 「エリア、ジャンル、店名」タブのxpath
-area_name_xpath = '//*[@id="__layout"]/div/div[2]/div/aside/div[2]/form/div/div/div/input'
-# 「🔍」ボタンのxpath
-search_botton_xpath = '//*[@id="__layout"]/div/div[2]/div/aside/div[2]/form/div/div/button'
+# 「銀座」ボタンのxpath
+ginza_xpath = '//*[@id="__layout"]/div/main/div[2]/section[3]/div[2]/nav[1]/a[2]'
 # 「クチコミ」ボタンのxpath
 review_xpath = '//*[@id="main-article"]/nav/ul/li[3]'
 # 「もっと見る」ボタンのxpath
--- lesson1/main.py
+++ lesson1/main.py
@@ -6,179 +6,77 @@ import pandas as pd
 import config
 def main():
   driver = webdriver.Chrome()
-  error_flg = False
+  return driver
+def open_target(driver):
+  """
+  toppageを開いて
+  「銀座」を開いて
+  レストランを開いて
+  クチコミを表示
+  """
   # 一休レストランのTOPページ
   driver.get(config.top_url)
   sleep(1)
-  # 「検索する」のボタンをクリック
-  if error_flg is False:
-    try:
-      notnow_button = driver.find_element_by_xpath(config.search_xpath)
-      sleep(1)
-      notnow_button.click()
-      sleep(1)
-    except Exception:
-      pass
-  # 「リッツカールトン京都」で検索
-  if error_flg is False:
-    try:
-      notnow_button = driver.find_element_by_xpath(config.area_name_xpath)
-      sleep(1)
-      notnow_button.click()
-      sleep(1)
-      notnow_button.send_keys('リッツカールトン京都')
-      sleep(1)
-      search_button = driver.find_element_by_xpath(config.search_botton_xpath)
-      sleep(1)
-      search_button.click()
-      sleep(1)
-    except Exception:
-      pass
-  # 「ロカンダ」をクリック
-  if error_flg is False:
-    try:
-      notnow_button = driver.find_element_by_xpath('//*[@id="__layout"]/div/div[2]/div/main/section[1]/a/div[2]/h3')
-      sleep(1)
-      notnow_button.click()
-      sleep(1)
-    except Exception:
-      pass
-  # 「クチコミ」をクリック
-  if error_flg is False:
-    try:
-      notnow_button = driver.find_element_by_xpath(config.review_xpath)
-      sleep(1)
-      notnow_button.click()
-      sleep(1)
-    except Exception:
-      pass
-  # 「もっと見る」をクリック
-  if error_flg is False:
-    try:
-      notnow_button = driver.find_element_by_xpath(config.more_xpath)
-      sleep(1)
-      notnow_button.click()
-      sleep(1)
-    except Exception:
-      pass
-  # 「先頭へ」をクリック
-  if error_flg is False:
-    try:
-      notnow_button = driver.find_element_by_xpath(congig.toppage_xpath)
-      sleep(1)
-      notnow_button.click()
-      sleep(1)
-    except Exception:
-      pass
-  # このページの評価（コメント、味、サービス）のスクレイピング
-  assesment_url = 'https://restaurant.ikyu.com/103396/review/'
+  # 「銀座」を押す
+  button = driver.find_element_by_xpath(config.ginza_xpath)
+  sleep(1)
+  button.click()
+  sleep(1)
+  # （一番）のレストランを取得
+  button = driver.find_element_by_xpath('//*[@id="__layout"]/div/div[2]/div[1]/main/section[1]/a/div[1]/span/img')
+  sleep(1)
+  butto.click()
+  sleep(1)
+  # クチコミを表示
+  a = driver.find_element_by_id('layout').text
+  if a != '規定評価数に達していません':
+    button = driver.find_element_by_xpath(config.review_xpath)
+    sleep(1)
+    button.click()
+    sleep(1)
+    button = driver.find_element_by_xpath(config.more_xpath)
+    sleep(1)
+    button.click()
+    sleep(1)
+    button = driver.find_element_by_xpath(congig.toppage_xpath)
+    sleep(1)
+    button.click()
+    sleep(1)
+def get_item():
+  """
+  コメント、味、サービスのスクレイピング
+  """
+  assesment_url = driver.current_url
   driver.implicitly_wait(3)
   driver.get(assesment_url)
+  assesments = driver.find_elements_by_class_name('des_gdIDUsrImprBox')
+  i = 4
+  for assesment in assesments:
+    if i == 24:
+      break
+    comment = assesment.find_element_by_xpath(f'//*[@id="des_inner"]/div[{i}]/div[3]/table/tbody/tr[3]/td').text
+    comments.append(comment)
+    taste = assesment.find_element_by_xpath(f'//*[@id="des_inner"]/div[{i}]/div[1]/table/tbody/tr[2]/td[2]/span').text
+    tastes.append(taste)
+    service = assesment.find_element_by_xpath(f'//*[@id="des_inner"]/div[{i}]/div[1]/table/tbody/tr[3]/td[2]/span').text
+    services.append(service)
+    i +=2
+  print(comments,tastes,services)
+  def writeCSV():
+    """
+    csvに保存
+    """
+    df = pd.DataFrame()
+    df[comment] = comments
+    df[taste] = tastes
+    df[service] = services
+    df.to_csv('assesment.csv', index=False)
+
+if __name__ == "__main__":
+  main()
+  open_target(driver)
+  get_item()
   comments = []
   tastes = []
   services = []
-  assesments = driver.find_elements_by_class_name('des_gdIDUsrImprBox')
-  i = 4
-  for assesment in assesments:
-    if i == 24:
-      break
-    comment = assesment.find_element_by_xpath(f'//*[@id="des_inner"]/div[{i}]/div[3]/table/tbody/tr[3]/td').text
-    comments.append(comment)
-    taste = assesment.find_element_by_xpath(f'//*[@id="des_inner"]/div[{i}]/div[1]/table/tbody/tr[2]/td[2]/span').text
-    tastes.append(taste)
-    service = assesment.find_element_by_xpath(f'//*[@id="des_inner"]/div[{i}]/div[1]/table/tbody/tr[3]/td[2]/span').text
-    services.append(service)
-    i +=2
-  print(comments,tastes,services)
-  df = pd.DataFrame()
-  df[comment] = comments
-  df[taste] = tastes
-  df[service] = services
-  df.to_csv('assesment.csv', index=False)
-  # 一休レストランのTOPページ
-  driver.get(config.top_url)
-  sleep(1)
-  # 「検索する」のボタンをクリック
-  if error_flg is False:
-    try:
-      notnow_button = driver.find_element_by_xpath(config.search_xpath)
-      sleep(1)
-      notnow_button.click()
-      sleep(1)
-    except Exception:
-      pass
-  # 「フォーシーズンズホテル京都」で検索
-  if error_flg is False:
-    try:
-      notnow_button = driver.find_element_by_xpath(config.area_name_xpath)
-      sleep(1)
-      notnow_button.click()
-      sleep(1)
-      notnow_button.send_keys('フォーシーズンズホテル京都')
-      sleep(1)
-      search_button = driver.find_element_by_xpath(config.search_botton_xpath)
-      sleep(1)
-      search_button.click()
-      sleep(1)
-    except Exception:
-      pass
-  # 「ブラッスリー」をクリック
-  if error_flg is False:
-    try:
-      notnow_button = driver.find_element_by_xpath('//*[@id="__layout"]/div/div[2]/div/main/section/a/div[2]/h3')
-      sleep(1)
-      notnow_button.click()
-      sleep(1)
-    except Exception:
-      pass
-  # 「クチコミ」をクリック
-  if error_flg is False:
-    try:
-      notnow_button = driver.find_element_by_xpath(config.review_xpath)
-      sleep(1)
-      notnow_button.click()
-      sleep(1)
-    except Exception:
-      pass
-  # 「もっと見る」をクリック
-  if error_flg is False:
-    try:
-      notnow_button = driver.find_element_by_xpath(config.more_xpath)
-      sleep(1)
-      notnow_button.click()
-      sleep(1)
-    except Exception:
-      pass
-  # 「先頭へ」をクリック
-  if error_flg is False:
-    try:
-      notnow_button = driver.find_element_by_xpath(config.toppage_xpath)
-      sleep(1)
-      notnow_button.click()
-      sleep(1)
-    except Exception:
-      pass
-  # このページの評価（コメント、味、サービス）のスクレイピング
-  assesment_url = 'https://restaurant.ikyu.com/107112/review/'
-  driver.implicitly_wait(3)
-  driver.get(assesment_url)
-  assesments = driver.find_elements_by_class_name('des_gdIDUsrImprBox')
-  i = 4
-  for assesment in assesments:
-    if i == 24:
-      break
-    comment = assesment.find_element_by_xpath(f'//*[@id="des_inner"]/div[{i}]/div[3]/table/tbody/tr[3]/td').text
-    comments.append(comment)
-    taste = assesment.find_element_by_xpath(f'//*[@id="des_inner"]/div[{i}]/div[1]/table/tbody/tr[2]/td[2]/span').text
-    tastes.append(taste)
-    service = assesment.find_element_by_xpath(f'//*[@id="des_inner"]/div[{i}]/div[1]/table/tbody/tr[3]/td[2]/span').text
-    services.append(service)
-    i +=2
-  print(comments,tastes,services)
-  df = pd.DataFrame()
-  df[comment] = comments
-  df[taste] = tastes
-  df[service] = services
-  df.to_csv('assesment.csv', index=False)
-if __name__ == "__main__":
-  main()
+  writeCSV()
